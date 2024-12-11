ceda80fcce1050e948944e4e2b125af2e9ce121c
Abdirahman2019 <57561471+Abdirahman2019@users.noreply.github.com>
2024-12-12 00:47:43 +0300

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,6 +1,6 @@
 # This code test the factorial of a number,whereby user enter a digit from the keyboard
 num = int(input("Enter a number = "))
-if num < 0:
+if num < 0: #check if the number is less than zero,-ve numbers can not be factorized
     print("The factorial can not be zero")
 elif num == 0 or num == 1:
     print("The factorial of",num,"is 1 ") 
