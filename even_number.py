be79ed459b6b9b006ee5caa2a8245e7f2f7db5f2
abdirahman2019 <abdiraxman106@gmail.com>
2024-12-11 23:33:10 +0300

    added odd/even number

---
+++ even_number.py
@@ -0,0 +1,8 @@
+#To chech a number if its even or odd number.
+
+n=int(input("Enter a number :"))
+if n % 2 == 1:
+   print("The value Entered is odd Number. ") 
+else:
+     
+     print("The value Enter is even Number.")  
