ca111efde052ee210e52da689cc707ccfd7ba724
abdirahman2019 <abdiraxman106@gmail.com>
2024-12-13 20:24:47 +0300

    Login_python

---
+++ Login.py
@@ -0,0 +1,17 @@
+print("***********************************")
+print("          LOGIN                    ")
+print("***********************************")
+def main_page():
+    print("WELCOME PAGE!!!")
+def login():
+    while True:
+        username = input("Enter Your Username: ")
+        password = input("Enter Your Password: ")
+        if username == "admin" and  password == "12345":
+            print("You Login successfully")
+            main_page()
+            break
+        else:
+            print("Wrong Username and password!!!")  
+login()   
+  
